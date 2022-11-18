3830da36690089c9b9fc7b6a3059a75e9a8a14d9
Gokul <gokulakrishnan1996@gmail.com>
2022-11-18 14:41:24 -0700

    added querysets

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
+from django.utils import timezone
 from django.shortcuts import render
-
+from .models import Post
+from django.contrib.auth.models import User
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    me = User.objects.get(username='gokul')
+    posts = Post.objects.filter(author = me).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
