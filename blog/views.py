0814fa2d76b0f93a910db248fb6ca19860403861
Gokul <gokulakrishnan1996@gmail.com>
2022-11-18 18:30:15 -0700

    Added CSS

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.contrib.auth.models import User
 # Create your views here.
@@ -8,3 +8,7 @@ def post_list(request):
     me = User.objects.get(username='gokul')
     posts = Post.objects.filter(author = me).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
